d95d31998d86b93394217cec3464c354b9d92bbc
Alex Maraio <am963@sussex.ac.uk>
2020-02-01 23:11:16 +0000

    Add method to compute and extract transfer functions from CAMB
    Signed-off-by: Alex Maraio <am963@sussex.ac.uk>

--- BoltzmannClass.py
+++ BoltzmannClass.py
@@ -9,8 +9,8 @@ that is used to calculate the transfer functions of the temperature
 anisotropy of the CMB.
 
 The goal here is to establish which code we are using (either CAMB
-or CLASS) and then provide a consistant interface for the main code
-as here we can deal with the specalisms of each code
+or CLASS) and then provide a consistent interface for the main code
+as here we can deal with the specialisms of each code
 """
 
 
@@ -35,11 +35,24 @@ class BoltzmannCode:
             self.params.set_cosmology(H0=cosmology.H0, ombh2=cosmology.omega_bh2,
                                       omch2=cosmology.omega_cdmh2, tau=cosmology.tau)
 
-
-
-
         elif code.lower() == 'class':
             raise RuntimeError('CLASS is currently not supported, please use CAMB instead.')
 
         else:
-            raise RuntimeError('Unkown Bolzmann code specified.')
+            raise RuntimeError('Unknown Boltzmann code specified.')
+
+        self.data = None
+        self.transfer = None
+
+    def compute_transfer(self, accuracy_level=5, lSampleBoost=2):
+        import camb
+        self.params.set_accuracy(AccuracyBoost=accuracy_level, lSampleBoost=lSampleBoost)
+        self.data = camb.get_transfer_functions(self.params)
+        self.transfer = self.data.get_cmb_transfer_data()
+
+    def get_transfer(self, ell, **kwargs):
+        if self.transfer is None:
+            self.compute_transfer(**kwargs)
+
+        return self.transfer.q, self.transfer.delta_p_l_k[0, ell, :]
+
