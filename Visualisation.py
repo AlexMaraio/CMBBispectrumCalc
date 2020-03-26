70f8f65bcea27118e4a6c713177f4a6170931044
Alex Maraio <alex.maraio@live.com>
2020-03-26 13:44:25 +0000

    Add new plot type 'equal_ell_bispectrum_plot'
    - Since the equal-ell bispectrum can be plotted easily in 1D, we include a plotting function for this data
    
    Signed-off-by: Alex Maraio <am963@sussex.ac.uk>

--- Visualisation.py
+++ Visualisation.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2020 University of Sussex.
 # contributor: Alessandro Maraio <am963@sussex.ac.uk>
 
+
 """
 This file will be where our visualisation tools will be put so that way
 we can present the results for the two- and three-point function integrals
@@ -12,16 +13,62 @@ of 3 variables, we will need to plot on a 3D grid and so some work will need to 
 """
 
 
-def twopf_visualisation(ell, c_ell, style='seaborn', useLaTeX=False):
-    # Default power spectrum plot of ell vs C_ell.
-    import matplotlib.pyplot as plt
-    if style == 'seaborn':
-        import seaborn as sns
-        sns.set(font_scale=2.25, rc={'text.usetex': True}) if useLaTeX else sns.set()
+import matplotlib.pyplot as plt
 
-    plt.figure(figsize=(13, 7))
+
+def twopf_visualisation(ell, c_ell, use_seaborn=True, use_LaTeX=False):
+    """
+    Function for creating a default power spectrum plot for C_ell vs ell, in standard plotting units.
+
+    Takes ell and c_ell, which are lists that corresponds to the integration data, and optional bool switches
+    on or off using the seaborn plotting style, and using LaTeX labels.
+    """
+
+    # If using seaborn, import it and set plot options given function inputs
+    if use_seaborn:
+        import seaborn as sns
+        sns.set(font_scale=1.75, rc={'text.usetex': True}) if use_LaTeX else sns.set(font_scale=1.5)
+
+    # Create a matplotlib figure and plot the power spectrum on it.
+    plt.figure(figsize=(16, 9))
     plt.loglog(ell, c_ell, lw=2.5)
     plt.xlabel(r'$\ell$')
-    plt.ylabel(r'$C_\ell \, \ell(\ell + 1)  / 2 \pi$ ')
-    plt.title(r'Custom $C_\ell$ plot for quadratic $\frac{1}{2} m^2 \phi^2$')
+
+    # Change the y-label depending on if we're using LaTeX labels or not
+    if use_LaTeX:
+        plt.ylabel(r'$C_\ell \,\, \ell(\ell + 1)  / 2 \pi \,\, [\mu \textrm{K}^2]$')
+    else:
+        plt.ylabel(r'$C_\ell \,\, \ell(\ell + 1)  / 2 \pi \,\, [\mu K^2]$')
+
+    plt.title(r'CMB TT power spectrum $C_\ell$ plot')
+    plt.tight_layout()
+    plt.show()
+
+
+def equal_ell_bispectrum_plot(ell, bispec, use_seaborn=True, use_LaTeX=False, save_folder=None):
+    """
+    Function that creates a default plot of the equal ell CMB bispectrum.
+
+    Takes arguments of the ell and bispec, which are lists that corresponds to the integration data,
+    two bool switches that determine plot styling options,
+    and an optional string save_folder, which if provided saves the figure into the folder given.
+    """
+
+    # Import seaborn if we are using it and initialise with plot settings determined by function inputs
+    if use_seaborn:
+        import seaborn as sns
+        sns.set(font_scale=1.75, rc={'text.usetex': True}) if use_LaTeX else sns.set(font_scale=1.5)
+
+    # Create a matplotlib figure and plot the bispectrum on it
+    plt.figure(figsize=(16, 9))
+    plt.semilogx(ell, bispec, lw=2.5)
+    plt.xlabel(r'$\ell$')
+    plt.ylabel(r'Bispectrum $b_{\ell \, \ell \, \ell}$')
+    plt.title(r'CMB TTT bispectrum plot for $\ell_1 = \ell_2 = \ell_3 \equiv \ell$')
+    plt.tight_layout()
+
+    # If a save folder is provided, save the figure to that folder.
+    if save_folder is not None:
+        plt.savefig(str(save_folder) + '/equal_ell_step.png', dpi=500)
+
     plt.show()
